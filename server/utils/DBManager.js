54667c80772e530a2e0865a05a63a0776a5800ae
mofath <mohamed.elgaedi@gmail.com>
2020-09-14 07:50:50 +0200

    Add DBManager
    DBManager is a module contains two functions; CONNECT and DISCONNECT to be used wherever we need to connect to the database and disconnect again after finishin the operation

---
+++ server/utils/DBManager.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const config = require('../config')
+
+const DB_URL = config.DB_URL
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}
+mongoose.Promise = global.Promise;
+
+const DBManager = {
+    CONNECT: async () => {
+        try {
+            const connection = await mongoose.connect(DB_URL, options)
+            if (connection) {
+                console.log('succcessfully connected to mongodb')
+                return connection;
+            }
+        } catch (error) {
+            console.log(`DB Connection Error: ${error.message}`)
+        }
+    },
+    DISCONNECT: async () => {
+        try {
+            return await mongoose.disconnect();
+        } catch (error) {
+            console.log(`DB disconnection Error: ${error.message}`)
+        }
+    },
+};
+
+
+module.exports = DBManager;
